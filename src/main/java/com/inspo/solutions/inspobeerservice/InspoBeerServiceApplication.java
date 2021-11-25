1b3ee81df49fe7a74ed3e0e4db6f8e07555f860e
Himadri Talukder <himadricuet@gmail.com>
2021-11-24 20:21:22 -0500

    Initial project structure

---
+++ src/main/java/com/inspo/solutions/inspobeerservice/InspoBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inspo.solutions.inspobeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspoBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspoBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inspo/solutions/inspobeerservice/InspoBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspo.solutions.inspobeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspoBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
